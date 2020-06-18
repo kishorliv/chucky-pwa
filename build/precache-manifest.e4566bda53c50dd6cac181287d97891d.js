ce28ef64c30a479a9563588ccc2dcd28a1ca0ed6
kishor neupane <kishorliv@gmail.com>
2020-06-19 02:30:41 +1000

    Project build.

---
+++ build/precache-manifest.e4566bda53c50dd6cac181287d97891d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231fc9b54017577d406e463b9cff5641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac8956f11038b8e5645a",
+    "url": "/static/css/main.ce4546f7.chunk.css"
+  },
+  {
+    "revision": "5d15880ce75da5866881",
+    "url": "/static/js/2.43c4d81f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43c4d81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8956f11038b8e5645a",
+    "url": "/static/js/main.7a699e2d.chunk.js"
+  },
+  {
+    "revision": "11b13456c33a29fe7472",
+    "url": "/static/js/runtime-main.eb71981f.js"
+  },
+  {
+    "revision": "22d0a3004c4230fd2f760ea838bd7f26",
+    "url": "/static/media/logo.22d0a300.png"
+  }
+]);
